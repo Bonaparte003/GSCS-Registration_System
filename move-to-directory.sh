c77c429e08a913e97dfe556740fb39fff6b5066b
milkakeza <m.isingizwe1@alustudent.com>
2024-06-11 07:27:47 -0700

    I exist

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -9,3 +9,12 @@ function loader {
     echo -e "100%\n"
 }
 
+# Check if the directory exists---------------------
+folder="negpod_12-q1"
+
+# File initialization---------------
+s_email='select-emails.sh'
+main='main.sh'
+file_path='Students-list_1023.txt'
+file_save='student-emails.txt'
+
